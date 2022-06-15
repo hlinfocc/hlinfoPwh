9853c742ab91c0a27ff8c24ceab40dff77db2175
hlinfo <kf39@hlinfo.net>
2022-06-15 21:35:13 +0800

    Initial commit

---
+++ init-pkg.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+go mod download github.com/astaxie/beego
+go get github.com/shiena/ansicolor
+go get github.com/astaxie/beego@v1.12.1
+go get github.com/astaxie/beego/context@v1.12.1
+
+
